cf1b5f415bec8a1378272f9261786893c03e7ec4
jardin22 <jardin@qstrike.com>
2022-02-15 16:41:51 +0800

    go

--- resources/views/posts/layout.blade.php
+++ resources/views/posts/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>asd CRUD Application</title>
+    <title>CRUD Application</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 </head>
     <body>
