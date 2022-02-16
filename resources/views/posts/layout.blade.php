557a1cd103734bc37953941ac55907936bd150b3
jardin22 <jardin@qstrike.com>
2022-02-16 10:03:54 +0800

    Ready for push

--- resources/views/posts/layout.blade.php
+++ resources/views/posts/layout.blade.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<<<<<<< HEAD
-    <title>CRUD Application Bidabida ang papalit neto</title>
-=======
     <title>CRUD Application Conflict Trial</title>
->>>>>>> TryToConflict
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 </head>
     <body>
