95839454ecc55269c7ce13be090f720e32a92008
jardin22 <jardin@qstrike.com>
2022-02-16 09:45:11 +0800

    Tinagalog na yan

--- resources/views/posts/layout.blade.php
+++ resources/views/posts/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>CRUD Application</title>
+    <title>CRUD Application Bidabida ang papalit neto</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 </head>
     <body>
